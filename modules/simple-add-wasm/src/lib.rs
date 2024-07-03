b31ecce7f5e02fd5408bc3275b3b99edea3e8577
tonymushah <tonyramanantsalama@gmail.com>
2024-07-03 22:11:32 +0300

    finished string sharing

--- modules/simple-add-wasm/src/lib.rs
+++ modules/simple-add-wasm/src/lib.rs
@@ -1,11 +1,11 @@
-extern {
+extern "C" {
     fn say(something: &str);
 }
 
 #[link(wasm_import_module = "utils")]
-extern {
+extern "C" {
     fn panic();
-} 
+}
 
 #[export_name = "add"]
 pub fn add(left: u32, right: u32) -> u32 {
@@ -14,9 +14,18 @@ pub fn add(left: u32, right: u32) -> u32 {
 
 #[export_name = "run"]
 pub fn run() {
-    unsafe { say("Hello World!") };
-    panic!("some panic");
-    //unsafe { panic() }
+    unsafe {
+        say("Hello World!");
+        say("Yes! it worked")
+    };
+    unsafe { panic() }
+}
+
+#[export_name = "tell"]
+pub fn tell(something: &str) {
+    let to_say_i = format!("Oh you just told me that `{something}`");
+    let to_say: &str = to_say_i.as_ref();
+    unsafe { say(to_say) }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,43 @@ use std::{
     env,
     fs::File,
     io::{BufReader, Read},
+    ptr::null,
+    sync::{Arc, Mutex},
 };
 
 use dotenvy::dotenv;
 use wasmer::{
-    imports, sys::EngineBuilder, wasmparser::Export, AsEngineRef, AsStoreMut, AsStoreRef, Engine,
-    Exports, Extern, ExternRef, Function, FunctionEnv, FunctionEnvMut, Global, Instance, Memory,
-    Memory32, Memory64, MemoryView, Module, NativeEngineExt, SharedMemory, Store, Type,
-    TypedFunction, WasmPtr, WasmTypeList,
+    imports,
+    sys::{EngineBuilder, Features},
+    wasmparser::{Export, Import},
+    AsEngineRef, AsStoreMut, AsStoreRef, Cranelift, Engine, Exports, Extern, ExternRef, Function,
+    FunctionEnv, FunctionEnvMut, Global, Imports, Instance, Memory, Memory32, Memory64, MemoryView,
+    Module, NativeEngineExt, RuntimeError, SharedMemory, Store, Type, TypedFunction, WasmPtr,
+    WasmTypeList,
 };
 
-fn say(mut env: FunctionEnvMut<()>, memory: Memory, string: WasmPtr<u8>, len: u32) {
-    todo!()
+#[derive(Debug, Clone)]
+struct SayEnv {
+    memory: Arc<Mutex<Option<Memory>>>,
+}
+fn say(
+    env: FunctionEnvMut<SayEnv>,
+    /*memory: &Memory,*/ string: WasmPtr<u8>,
+    len: u32,
+) -> Result<(), RuntimeError> {
+    let Ok(mem) = env.data().memory.lock() else {
+        return Err(RuntimeError::new("Memory access error"));
+    };
+
+    let Some(memory) = mem.as_ref() else {
+        return Err(RuntimeError::new("Memory not defined"));
+    };
+
+    let view = memory /*.memory() */
+        .view(&env);
+    let str = string.read_utf8_string(&view, len)?;
+    println!("{str}");
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
@@ -32,6 +57,17 @@ fn main() -> anyhow::Result<()> {
     //
     // Most WebAssembly programs come from compiling source code in a high level
     // language and will already be in the binary format.
+    let compiler = Cranelift::new();
+    let engine = EngineBuilder::new(compiler)
+        .set_features(Some(Features {
+            memory64: true,
+            ..Default::default()
+        }))
+        .engine();
+    // Create a Store.
+    let mut store = Store::new(engine);
+    // We then use our store and Wasm bytes to compile a `Module`.
+    // A `Module` is a compiled WebAssembly module that isn't ready to execute yet.
     let wasm_bytes = {
         let file = env::var("SIMPLE_ADD")?;
         let mut bytes = Vec::new();
@@ -39,11 +75,6 @@ fn main() -> anyhow::Result<()> {
         reader.read_to_end(&mut bytes)?;
         bytes
     };
-    // Create a Store.
-    let mut store = Store::default();
-
-    // We then use our store and Wasm bytes to compile a `Module`.
-    // A `Module` is a compiled WebAssembly module that isn't ready to execute yet.
     let module = Module::new(&store, wasm_bytes)?;
 
     // We define a function to act as our "env" "say_hello" function imported in the
@@ -51,8 +82,10 @@ fn main() -> anyhow::Result<()> {
     /*fn say_hello_world() {
         println!("Hello, world!")
     }*/
-    // struct SayEnv;
-    let say_env = FunctionEnv::new(&mut store, ());
+    let say_env_ins = SayEnv {
+        memory: Arc::new(Mutex::new(None)),
+    };
+    let say_env = FunctionEnv::new(&mut store, say_env_ins.clone());
 
     let say_fn = Function::new_typed_with_env(&mut store, &say_env, say);
     // We then create an import object so that the `Module`'s imports can be satisfied.
@@ -71,12 +104,16 @@ fn main() -> anyhow::Result<()> {
     // An `Instance` is a compiled WebAssembly module that has been set up
     // and is ready to execute.
     let instance = Instance::new(&mut store, &module, &import_object)?;
-
+    let ins_memory = instance.exports.get_memory("memory")?;
+    if let Ok(mut memory) = say_env_ins.memory.lock() {
+        memory.replace(ins_memory.clone());
+    }
     // We get the `TypedFunction` with no parameters and no results from the instance.
     //
     // Recall that the Wasm module exported a function named "run", this is getting
     // that exported function from the `Instance`.
     let add: TypedFunction<(u32, u32), u32> = instance.exports.get_typed_function(&store, "add")?;
+
     // Finally, we call our exported Wasm function which will call our "say_hello"
     // function and return.
     let res = add.call(&mut store, 1, 4)?;
@@ -85,5 +122,20 @@ fn main() -> anyhow::Result<()> {
     let run: TypedFunction<(), ()> = instance.exports.get_typed_function(&store, "run")?;
 
     run.call(&mut store)?;
+    {
+        let tell: TypedFunction<(WasmPtr<u8, Memory32>, u32), ()> = instance
+            .exports
+            .get_typed_function(&store, "tell")
+            .expect("the error is here");
+        let store_view = ins_memory.view(&store);
+        let to_tell = "Rust and Wasmer is awesome";
+        store_view.write(0, to_tell.as_bytes())?;
+        let ptr = WasmPtr::<u8, Memory32>::new(0);
+        if let Ok(mut memory) = say_env_ins.memory.lock() {
+            memory.replace(ins_memory.clone());
+        }
+        tell.call(&mut store, ptr, to_tell.len() as u32)?;
+    }
+
     Ok(())
 }
